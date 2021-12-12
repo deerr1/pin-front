037391bc3f99a2b37353ceaf24cb50236b7ce7e9
deerr1 <furagkin678@gmail.com>
2021-12-13 02:11:43 +0500

    fix port

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ createApp(App)
   .use(router)
   .mount("#app");
 
-axios.defaults.baseURL = "http://localhost:8000/api/";
+axios.defaults.baseURL = "http://localhost:8081/api/";
 const token = localStorage.getItem('access_token')
 if (token){
   axios.defaults.headers.common['Authorization'] = "Bearer " + token
